1f010a693e33b4adc301261c84f54f709182cdf2
Hiirsh <47283490+Hiirsh@users.noreply.github.com>
2022-03-28 02:43:17 +0300

    Deleted pointer from title of extended event (changed Event, App.css

--- src/components/Main/Event.jsx
+++ src/components/Main/Event.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
-import {eventPage} from '../../utils/constants';
+import {eventPage, homePage} from '../../utils/constants';
 import EventMainInfo from './EventMainInfo';
 import style from '..//..//componentStyles/Event.css';
 
@@ -10,17 +10,21 @@ export default function Event(props) {
     const isExtended = props.extended;
     return (
         <div>
-            <h1
-                className="eventTitle"
-                onClick={() => navigate(`/${eventPage}/${ev.id}`)}
-            >{`${ev.title}`}</h1>
-       
+            {isExtended ? (
+                <h1 className="eventTitle ">{`${ev.title}`}</h1>
+            ) : (
+                <h1
+                    className="eventTitle titleActive"
+                    onClick={() => navigate(`/${eventPage}/${ev.id}`)}
+                >{`${ev.title}`}</h1>
+            )}
             <EventMainInfo ev={ev} />
-            <div className="text-left m-2"><p>{`${ev.preview}`}</p></div>
+            <div className="text-left m-2">
+                <p>{`${ev.preview}`}</p>
+            </div>
             <div className="text-left m-2">
                 <p>{`${isExtended ? ev.description : ''}`}</p>
             </div>
         </div>
     );
-
 }

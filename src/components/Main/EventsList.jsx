22a456bf2352fe220169fcc37ba24e14e353cd7d
Hiirsh <47283490+Hiirsh@users.noreply.github.com>
2022-03-27 21:31:25 +0300

    разрешил конфлитк версий

--- src/components/Main/EventsList.jsx
+++ src/components/Main/EventsList.jsx
@@ -1,16 +1,27 @@
-import React, {useState} from 'react';
-import {useSelector} from 'react-redux';
-import Event from './Event';
 import styles from '..//..//componentStyles/EventList.css';
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {changeEventList} from '../../reduxFiles/actions/changePageStateAction';
+import Event from './Event';
 
 export default function Events() {
-    const [isCollapced, setCollapse] = useState(true);
-    const [renderFirstEvent, setRenderFirstEvent] = useState(0);
-    const [renderLastEvent, setRenderLastEvent] = useState(2);
+    // const [isCollapced, setCollapse] = useState(true);
+    // const [renderFirstEvent, setRenderFirstEvent] = useState(0);
+    // const [renderLastEvent, setRenderLastEvent] = useState(2);
+    // const {events} = useSelector(state => state.events);
+    // let numEvents = events.length;
+    // const eventsRender = events.slice(renderFirstEvent, renderLastEvent);
     const {events} = useSelector(state => state.events);
+    const [renderFirstEvent, setRenderFirstEvent] = useState(0);
+    const isCollapced = useSelector(
+        state => state.pageState.eventListCollapced
+    );
     let numEvents = events.length;
+    const [renderLastEvent, setRenderLastEvent] = useState(
+        isCollapced ? 2 : numEvents
+    );
     const eventsRender = events.slice(renderFirstEvent, renderLastEvent);
-
+    const dispatch = useDispatch();
     return (
         <div>
             <div className="d-grid gap-2 m-3">
@@ -21,11 +32,7 @@ export default function Events() {
             <div className="list-group">
                 {eventsRender.map((item, index) => (
                     <div className="eventInList" key={index}>
-                        <Event
-                            ev={item}
-                            extended={false}
-                            
-                        />
+                        <Event ev={item} extended={false} />
                     </div>
                 ))}
             </div>
@@ -36,7 +43,7 @@ export default function Events() {
                     onClick={e => {
                         e.preventDefault();
                         setRenderLastEvent(!isCollapced ? 2 : numEvents);
-                        setCollapse(!isCollapced);
+                        dispatch(changeEventList());
                     }}
                 >
                     {`${isCollapced ? 'Show all' : 'Hide'}`}
@@ -46,5 +53,3 @@ export default function Events() {
         </div>
     );
 }
-
-

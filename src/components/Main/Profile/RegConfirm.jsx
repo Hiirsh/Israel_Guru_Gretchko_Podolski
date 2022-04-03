ea16818b8b7d8594aba83a1c9a44173be50ec8c2
Hiirsh <47283490+Hiirsh@users.noreply.github.com>
2022-04-03 09:57:29 +0300

    03.04.2022

---
+++ src/components/Main/Profile/RegConfirm.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function RegConfirm(props) {
+    return <div>ConfirmSignUpPage</div>;
+}
---
+++ src/firebaseFiles/services/authService.jsx
@@ -0,0 +1,30 @@
+import 'firebase/compat/auth';
+import {fb} from '../config/firebaseConfig';
+export async function login(email, password) {
+    try {
+        const responce = await fb
+            .auth()
+            .signInWithEmailAndPassword(email, password);
+        return true;
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}
+
+export function logout() {
+    fb.auth().signOut();
+}
+
+export async function registration(email, password) {
+    try {
+        const responce = await fb
+            .auth()
+            .createUserWithEmailAndPassword(email, password);
+        console.log(responce.user);
+        return true;
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}

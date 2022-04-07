1fdf733efac2753cf9846507894b1e14865f47e8
Hiirsh <47283490+Hiirsh@users.noreply.github.com>
2022-04-07 10:10:26 +0300

    07/04/2022

--- src/components/Main/Profile/RegConfirm.jsx
+++ src/components/Main/Profile/RegConfirm.jsx
@@ -1,5 +1,22 @@
+import {Button} from '@mui/material';
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {accountPage} from '../../../utils/constants';
 
-export default function RegConfirm(props) {
-    return <div>ConfirmSignUpPage</div>;
+export default function RegConfirm() {
+    const userId = useSelector(state => state.userId);
+    const navigate = useNavigate();
+    return (
+        <div>
+            <h1>Registration Comfirmed!</h1>
+            <Button
+                variant="contained"
+                className="buttonStyle"
+                onClick={() => navigate(`../${accountPage}/${userId}`)}
+            >
+                To account
+            </Button>
+        </div>
+    );
 }
